c5a845490b62936a80fdf0ca947186975232dfa8
Janvi Patidar <janvilakhanpatidar@gmail.com>
2025-12-01 23:19:43 +0530

    ai resume builder

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import {getUserById,getUserResumes,loginUser,registerUser} from '../controllers/UserControllers.js'
+
+import protect from '../middlewares/authMiddleware.js';
+
+const userRouter=express.Router();
+
+userRouter.post('/register' , registerUser);
+userRouter.post('/login' , loginUser);
+userRouter.get('/data' , getUserById);
+userRouter.get('/resumes' , protect,getUserResumes)
+
+export default userRouter
